9c205c12129f761aef498ba44431dff155132592
winston <iwinston@163.com>
2019-10-18 23:06:41 +0800

    init tag module

---
+++ api/src/tag/tag.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TagController } from './tag.controller';
+import { TagService } from './tag.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tag } from './tag.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Tag])],
+  controllers: [TagController],
+  providers: [TagService],
+  exports: [TagService],
+})
+export class TagModule {}
