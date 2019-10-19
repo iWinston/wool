e83703af0afd07a6ee1b80d42be9b9e5aa49cca3
winston <iwinston@163.com>
2019-10-19 22:23:49 +0800

    commented api: patch user

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Patch, Body, Post, UseInterceptors } from '@nestjs/common';
+import { Controller, Patch, Body, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiUseTags, ApiOperation } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { userParam } from '@common/decorator/user.decorator';
@@ -11,11 +11,11 @@ import { PointDto } from './point.dto';
 @Controller('user')
 export class UserController {
   constructor(readonly userService: UserService) {}
-  @ApiOperation({ title: '修改用户信息' })
-  @Patch()
-  patch(@userParam() user: User, @Body() dto: UserProfileDto) {
-    return this.userService.patch(user, dto);
-  }
+  // @ApiOperation({ title: '修改用户信息' })
+  // @Patch()
+  // patch(@userParam() user: User, @Body() dto: UserProfileDto) {
+  //   return this.userService.patch(user, dto);
+  // }
 
   @ApiOperation({ title: '增加用户积分' })
   @Post('point')
